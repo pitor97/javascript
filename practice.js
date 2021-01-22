7e87fc287da3d015a2f099ab6047cb6f066b6c19
pitor97 <uzzolbesra@gmail.com>
2021-01-22 08:23:38 +0100

    first problem

--- practice.js
+++ practice.js
@@ -1,12 +1,6 @@
-//https://github.com/pitor97/javascript
-
-// Problem n1-kilometerToMeter
-
-//we know 1 kilometer = 1000 meter, so 
-
 function kilometerToMeter(kilometer){
     var meter = kilometer*1000;
     return meter;
 }
 var meter = kilometerToMeter(8);
-
+console.log(meter);
